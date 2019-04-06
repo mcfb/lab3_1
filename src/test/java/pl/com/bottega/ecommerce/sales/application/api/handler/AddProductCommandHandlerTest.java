4d9423f6a5428b9c556baf8a1bfb5ea2cff746cd
Fabicki.Maciej <209305@edu.p.lodz.pl>
2019-04-06 17:22:58 +0200

    test to check if product is added correctly

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,60 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.internal.util.reflection.Whitebox;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.Client;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservedProduct;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+public class AddProductCommandHandlerTest {
+
+
+    @Test
+    public void shouldLoadThenAddProductAndSaveReservationOnce() {
+
+        AddProductCommandHandler handler = new AddProductCommandHandler();
+        Product product = new Product(new Id("1"), new Money(10), "myProduct", ProductType.STANDARD);
+        Reservation reservation = mock(Reservation.class);
+        when(reservation.getStatus()).thenReturn(Reservation.ReservationStatus.OPENED);
+        when(reservation.isClosed()).thenReturn(Boolean.FALSE);
+        when(reservation.getClientData()).thenReturn(new ClientData(Id.generate(), "Client2"));
+        when(reservation.getCreateDate()).thenReturn(new Date());
+
+        ReservationRepository reservationRepository = mock(ReservationRepository.class);
+        Whitebox.setInternalState(handler, "reservationRepository", reservationRepository);
+        when(reservationRepository.load(any(Id.class))).thenReturn(reservation);
+
+        ProductRepository productRepository = mock(ProductRepository.class);
+        Whitebox.setInternalState(handler, "productRepository", productRepository);
+        when(productRepository.load(any(Id.class))).thenReturn(product);
+
+        AddProductCommand command = new AddProductCommand(new Id("1"), new Id("1"), 1);
+        handler.handle(command);
+        verify(reservationRepository, times(1)).load(any(Id.class));
+        verify(reservation, times(1)).add(product, 1);
+        verify(reservationRepository, times(1)).save(any(Reservation.class));
+
+    }
+
+
+}
